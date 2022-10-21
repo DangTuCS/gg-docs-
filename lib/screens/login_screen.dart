d56415458d2f623e997bcb97f25303758af0c28a
DangTuCS <dangtu12350@gmail.com>
2022-10-22 00:48:30 +0700

    creating and setting up Node server

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gg_docs/color.dart';
+import 'package:gg_docs/repository/auth_repository.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
+  void singInWithGoogle(WidgetRef ref){
+    ref.read(authRepositoryProvider).signInWithGoogle();
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Center(
         child: ElevatedButton.icon(
-          onPressed: () {},
+          onPressed: () => singInWithGoogle(ref),
           icon: Image.asset(
             'assets/images/g-logo-2.png',
             height: 20,
